afda19bcf6b1075ab8c7f7686bb7ce6e0976d151
d0rbu <henryandrecastillo@gmail.com>
2025-09-04 14:32:58 -0500

    fix download scripts

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -8,16 +8,19 @@ from exp import DATASET_DIRNAME
 
 @arguably.command()
 def download(hf_name: str = "lmsys/lmsys-chat-1m", revision: str | None = None) -> None:
+    local_dir = os.path.join(DATASET_DIRNAME, hf_name)
+    os.makedirs(local_dir, exist_ok=True)
+
     if revision is None:
         snapshot_download(
             repo_id=hf_name,
-            local_dir=os.path.join(DATASET_DIRNAME, hf_name),
+            local_dir=local_dir,
             repo_type="dataset",
         )
     else:
         snapshot_download(
             repo_id=hf_name,
-            local_dir=os.path.join(DATASET_DIRNAME, hf_name),
+            local_dir=local_dir,
             revision=revision,
             repo_type="dataset",
         )
--- scripts/download_model.py
+++ scripts/download_model.py
@@ -8,16 +8,19 @@ from exp import MODEL_DIRNAME
 
 @arguably.command()
 def download(hf_name: str = "allenai/OLMoE-1B-7B-0125-Instruct", revision: str | None = None) -> None:
+    local_dir = os.path.join(MODEL_DIRNAME, hf_name)
+    os.makedirs(local_dir, exist_ok=True)
+
     if revision is None:
         snapshot_download(
             repo_id=hf_name,
-            local_dir=os.path.join(MODEL_DIRNAME, hf_name),
+            local_dir=local_dir,
             repo_type="model",
         )
     else:
         snapshot_download(
             repo_id=hf_name,
-            local_dir=os.path.join(MODEL_DIRNAME, hf_name),
+            local_dir=local_dir,
             revision=revision,
             repo_type="model",
         )
