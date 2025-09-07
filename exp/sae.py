d9c3783b2bfaa1c378ba5b0339de449d08d6aaea
d0rbu <henryandrecastillo@gmail.com>
2025-09-06 23:09:02 -0500

    dont clobber saes when running multi node

--- exp/sae.py
+++ exp/sae.py
@@ -1,5 +1,7 @@
+import asyncio
+from collections.abc import Iterator
 from dataclasses import dataclass
-from itertools import chain, cycle, partial, product
+from itertools import batched, chain, islice, product
 import os
 
 import arguably
@@ -11,7 +13,10 @@ from dictionary_learning.trainers.matryoshka_batch_top_k import (
 from dictionary_learning.trainers.top_k import BatchTopKSAE, BatchTopKTrainer
 from dictionary_learning.trainers.trainer import SAETrainer
 from dictionary_learning.training import trainSAE
+from loguru import logger
 import torch as th
+import torch.distributed as dist
+from tqdm import tqdm
 
 from exp import OUTPUT_DIR
 from exp.activations import Activations
@@ -36,12 +41,244 @@ ARCHITECTURES = {
 }
 
 
+@dataclass
+class GPUBatch:
+    trainer_cfgs: list[dict]
+    trainer_names: list[str]
+    steps: int
+    num_epochs: int
+    sae_experiment_name: str
+
+
+MAX_GPU_QUEUE_SIZE = 2
+
+
+async def gpu_worker(
+    device_idx: int,
+    gpu_queue: asyncio.Queue,
+    data_iterator: Iterator[dict],
+) -> None:
+    """Worker process for training SAE models on a specific GPU."""
+
+    logger.info(f"Starting GPU worker {device_idx}")
+
+    while True:
+        batch: GPUBatch | None = await gpu_queue.get()
+        if batch is None:
+            break
+
+        await trainSAE(
+            data=data_iterator,
+            trainer_configs=batch.trainer_cfgs,
+            steps=batch.steps * batch.num_epochs,
+            trainer_names=batch.trainer_names,
+            use_wandb=False,
+            save_dir=os.path.join(OUTPUT_DIR, batch.sae_experiment_name),
+            normalize_activations=True,
+        )
+
+
 @arguably.command()
-def run_sae_training(
+async def run_sae_training(
     model_name: str = "olmoe-i",
     dataset_name: str = "lmsys",
     *_args,
     batch_size: int = 4096,
+    trainers_per_gpu: int = 2,
+    steps: int = 1024 * 256,
+    num_epochs: int = 1,
+    expansion_factor: list[int],
+    k: list[int],
+    layer: list[int],
+    group_fractions: list[list[float]],
+    group_weights: list[list[float] | None],
+    architecture: list[str],
+    lr: list[float],
+    auxk_alpha: list[float],
+    warmup_steps: list[int],
+    decay_start: list[int | None],
+    threshold_beta: list[float],
+    threshold_start_step: list[int],
+    k_anneal_steps: list[int | None],
+    seed: list[int],
+    submodule_name: list[str],
+    tokens_per_file: int = 10_000,
+) -> None:
+    """Train autoencoders to sweep over the given hyperparameter sets."""
+    activations_experiment_name = get_experiment_name(
+        model_name=model_name,
+        dataset_name=dataset_name,
+        tokens_per_file=tokens_per_file,
+    )
+
+    sae_experiment_name = get_experiment_name(
+        model_name=model_name,
+        dataset_name=dataset_name,
+        batch_size=batch_size,
+        steps=steps,
+        num_epochs=num_epochs,
+    )
+
+    rank = int(os.environ.get("SLURM_PROCID", 0))
+    world_size = int(os.environ.get("SLURM_NTASKS", 1))
+    dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+
+    activations = Activations(
+        experiment_name=activations_experiment_name,
+        seed=seed,
+    )
+
+    activation_dim = th.zeros(1, dtype=th.int32)
+
+    # load a batch of activations to get the dimension
+    data_iterable = activations(batch_size=1)
+    activation = next(data_iterable)
+    activation_dim[0] = activation[submodule_name[0]].shape[1]
+
+    # clean up the background worker and queue
+    data_iterable.send("STOP!")
+
+    activation_dim = activation_dim.item()
+
+    assert activation_dim > 0, "Activation dimension must be greater than 0"
+    assert num_epochs > 0, "Number of epochs must be greater than 0"
+
+    # to train for multiple epochs, we just repeat the data iterator
+    def data_iterator():
+        yield from chain(
+            *[activations(batch_size=batch_size) for _ in range(num_epochs)]
+        )
+
+    base_trainer_cfg = {
+        "steps": steps,
+        "activation_dim": activation_dim,
+        "lm_name": model_name,
+        "wandb_name": model_name,
+    }
+
+    num_gpus = th.cuda.device_count()
+    gpu_queues = [asyncio.Queue(maxsize=MAX_GPU_QUEUE_SIZE) for _ in range(num_gpus)]
+
+    workers = [
+        asyncio.create_task(
+            gpu_worker(device_idx, gpu_queues[device_idx], data_iterator)
+        )
+        for device_idx in range(num_gpus)
+    ]
+
+    hparam_sweep_iterator = list(
+        enumerate(
+            product(
+                expansion_factor,
+                k,
+                layer,
+                group_fractions,
+                group_weights,
+                architecture,
+                lr,
+                auxk_alpha,
+                warmup_steps,
+                decay_start,
+                threshold_beta,
+                threshold_start_step,
+                k_anneal_steps,
+                seed,
+                submodule_name,
+            )
+        )
+    )
+
+    # assign a subset of the hparam sweep to each rank
+    distributed_iterator = list(
+        islice(
+            hparam_sweep_iterator,
+            start=dist.get_rank(),
+            stop=None,
+            step=dist.get_world_size(),
+        )
+    )
+    distributed_iterator = tqdm(
+        distributed_iterator,
+        desc=f"Rank {dist.get_rank()}",
+        total=len(distributed_iterator),
+        leave=True,
+        position=dist.get_rank() * (num_gpus + 1),
+    )
+    # batch it based on how many trainers will be on each gpu
+    concurrent_trainer_batched_iterator = batched(
+        distributed_iterator, trainers_per_gpu
+    )
+
+    for trainer_batch in concurrent_trainer_batched_iterator:
+        # decide device_idx based on how full the gpu queues are
+        device_idx = th.argmin(th.tensor([q.qsize() for q in gpu_queues])).item()
+
+        trainer_cfgs = []
+        trainer_names = []
+
+        for hparam_idx, (
+            current_expansion_factor,
+            current_k,
+            current_layer,
+            current_group_fractions,
+            current_group_weights,
+            current_architecture,
+            current_lr,
+            current_auxk_alpha,
+            current_warmup_steps,
+            current_decay_start,
+            current_threshold_beta,
+            current_threshold_start_step,
+            current_k_anneal_steps,
+            current_seed,
+            current_submodule_name,
+        ) in trainer_batch:
+            trainer_names.append(f"{hparam_idx}_{len(hparam_sweep_iterator)}")
+
+            architecture_config = ARCHITECTURES[current_architecture]
+
+            trainer_cfg = {
+                **base_trainer_cfg,
+                "expansion_factor": current_expansion_factor,
+                "k": current_k,
+                "layer": current_layer,
+                "group_fractions": current_group_fractions,
+                "group_weights": current_group_weights,
+                "lr": current_lr,
+                "dict_class": architecture_config.sae,
+                "auxk_alpha": current_auxk_alpha,
+                "warmup_steps": current_warmup_steps,
+                "decay_start": current_decay_start,
+                "threshold_beta": current_threshold_beta,
+                "threshold_start_step": current_threshold_start_step,
+                "k_anneal_steps": current_k_anneal_steps,
+                "seed": current_seed,
+                "submodule_name": current_submodule_name,
+                "device": f"cuda:{device_idx}",
+            }
+            trainer_cfgs.append(trainer_cfg)
+
+        batch = GPUBatch(
+            trainer_cfgs=trainer_cfgs,
+            trainer_names=trainer_names,
+            sae_experiment_name=sae_experiment_name,
+            steps=steps,
+            num_epochs=num_epochs,
+        )
+        gpu_queues[device_idx].put(batch)
+
+    # put a sentinel value in the gpu queues to stop the workers
+    for gpu_queue in gpu_queues:
+        gpu_queue.put(None)
+
+
+@arguably.command()
+def main(
+    model_name: str = "olmoe-i",
+    dataset_name: str = "lmsys",
+    *_args,
+    batch_size: int = 4096,
+    trainers_per_gpu: int = 2,
     steps: int = 1024 * 256,
     num_epochs: int = 1,
     expansion_factor: list[int] | int = 16,
@@ -120,117 +357,33 @@ def run_sae_training(
     ), "Invalid architecture"
     assert len(submodule_name) > 0, "Submodule name is an empty list!"
 
-    activations_experiment_name = get_experiment_name(
-        model_name=model_name,
-        dataset_name=dataset_name,
-        tokens_per_file=tokens_per_file,
+    asyncio.run(
+        run_sae_training(
+            model_name=model_name,
+            dataset_name=dataset_name,
+            batch_size=batch_size,
+            trainers_per_gpu=trainers_per_gpu,
+            steps=steps,
+            num_epochs=num_epochs,
+            expansion_factor=expansion_factor,
+            k=k,
+            layer=layer,
+            group_fractions=group_fractions,
+            group_weights=group_weights,
+            architecture=architecture,
+            lr=lr,
+            auxk_alpha=auxk_alpha,
+            warmup_steps=warmup_steps,
+            decay_start=decay_start,
+            threshold_beta=threshold_beta,
+            threshold_start_step=threshold_start_step,
+            k_anneal_steps=k_anneal_steps,
+            seed=seed,
+            submodule_name=submodule_name,
+            tokens_per_file=tokens_per_file,
+        )
     )
 
-    activations = Activations(
-        experiment_name=activations_experiment_name,
-        seed=seed,
-    )
-    one_epoch_generator = partial(activations, batch_size=batch_size)
 
-    activation_dim = th.zeros(1, dtype=th.int32)
-
-    # load a batch of activations to get the dimension
-    data_iterable = one_epoch_generator()
-    activation = next(data_iterable)
-    activation_dim[0] = activation[submodule_name[0]].shape[1]
-
-    # clean up the background worker and queue
-    data_iterable.send("STOP!")
-
-    activation_dim = activation_dim.item()
-
-    assert activation_dim > 0, "Activation dimension must be greater than 0"
-    assert num_epochs > 0, "Number of epochs must be greater than 0"
-
-    # to train for multiple epochs, we just repeat the data iterator
-    data_iterator = chain(*[one_epoch_generator() for _ in range(num_epochs)])
-
-    base_trainer_cfg = {
-        "steps": steps,
-        "activation_dim": activation_dim,
-        "lm_name": model_name,
-        "wandb_name": model_name,
-    }
-
-    trainer_cfgs = []
-
-    num_gpus = th.cuda.device_count()
-
-    for device_idx, (
-        current_expansion_factor,
-        current_k,
-        current_layer,
-        current_group_fractions,
-        current_group_weights,
-        current_architecture,
-        current_lr,
-        current_auxk_alpha,
-        current_warmup_steps,
-        current_decay_start,
-        current_threshold_beta,
-        current_threshold_start_step,
-        current_k_anneal_steps,
-        current_seed,
-        current_submodule_name,
-    ) in cycle(range(num_gpus), product(
-        expansion_factor,
-        k,
-        layer,
-        group_fractions,
-        group_weights,
-        architecture,
-        lr,
-        auxk_alpha,
-        warmup_steps,
-        decay_start,
-        threshold_beta,
-        threshold_start_step,
-        k_anneal_steps,
-        seed,
-        submodule_name,
-    )):
-        architecture_config = ARCHITECTURES[current_architecture]
-
-        trainer_cfg = {
-            **base_trainer_cfg,
-            "expansion_factor": current_expansion_factor,
-            "k": current_k,
-            "layer": current_layer,
-            "group_fractions": current_group_fractions,
-            "group_weights": current_group_weights,
-            "lr": current_lr,
-            "dict_class": architecture_config.sae,
-            "auxk_alpha": current_auxk_alpha,
-            "warmup_steps": current_warmup_steps,
-            "decay_start": current_decay_start,
-            "threshold_beta": current_threshold_beta,
-            "threshold_start_step": current_threshold_start_step,
-            "k_anneal_steps": current_k_anneal_steps,
-            "seed": current_seed,
-            "submodule_name": current_submodule_name,
-            "device": f"cuda:{device_idx}",
-        }
-        trainer_cfgs.append(trainer_cfg)
-
-    sae_experiment_name = get_experiment_name(
-        model_name=model_name,
-        dataset_name=dataset_name,
-        batch_size=batch_size,
-        steps=steps,
-        num_epochs=num_epochs,
-    )
-
-    # train the sparse autoencoder (SAE)
-    trainSAE(
-        data=data_iterator,
-        trainer_configs=trainer_cfgs,
-        steps=steps * num_epochs,
-        use_wandb=False,
-        save_dir=os.path.join(OUTPUT_DIR, sae_experiment_name),
-        normalize_activations=True,
-    )
+if __name__ == "__main__":
+    arguably.run()
