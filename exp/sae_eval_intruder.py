ddb80ded6144dc54e5298c7aad683302b1e49560
d0rbu <henryandrecastillo@gmail.com>
2025-09-19 13:29:39 -0500

    start on loading dictionary_learning saes

--- exp/sae_eval_intruder.py
+++ exp/sae_eval_intruder.py
@@ -1,9 +1,14 @@
+from collections.abc import Callable
+import json
 from pathlib import Path
+import sys
 
 import arguably
+from loguru import logger
 from transformers import (
     AutoModel,
     BitsAndBytesConfig,
+    PreTrainedModel,
 )
 
 from core.dtype import get_dtype
@@ -13,6 +18,12 @@ from delphi.__main__ import populate_cache
 from delphi.config import CacheConfig, RunConfig
 from delphi.sparse_coders.sparse_model import non_redundant_hookpoints
 from exp import OUTPUT_DIR
+
+
+def load_hookpoints_and_saes(
+    model: PreTrainedModel, sae_base_path: Path
+) -> dict[str, Callable]:
+    raise NotImplementedError("Implement load_hookpoints_and_saes")
 
 
 @arguably.command()
@@ -27,9 +38,17 @@ def main(
     n_tokens: int = 10_000_000,
     batchsize: int = 8,
     n_latents: int = 1000,
+    expansion_factor: int = 16,
+    k: int = 160,
+    layer: int = 7,
+    architecture: str = "batchtopk",
     seed: int = 0,
     hf_token: str = "",
+    log_level: str = "INFO",
 ) -> None:
+    logger.remove()
+    logger.add(sys.stderr, level=log_level)
+
     model_config = get_model_config(model_name)
     model_ckpt = model_config.get_checkpoint_strict(step=model_step_ckpt)
     dtype = get_dtype(model_dtype)
@@ -39,11 +58,57 @@ def main(
         quantization_config = BitsAndBytesConfig(load_in_8bit=load_in_8bit)
 
     root_dir = Path(OUTPUT_DIR, experiment_dir)
-    base_path = root_dir / "delphi"
+
+    matching_sae_dirpaths = []
+    for sae_dirpath in root_dir.iterdir():
+        if not sae_dirpath.is_dir():
+            continue
+
+        config_path = sae_dirpath / "config.json"
+        if not config_path.is_file():
+            continue
+
+        with open(config_path) as f:
+            config = json.load(f)
+
+        if config["expansion_factor"] != expansion_factor:
+            continue
+
+        if config["k"] != k:
+            continue
+
+        if config["layer"] != layer:
+            continue
+
+        if config["architecture"] != architecture:
+            continue
+
+        matching_sae_dirpaths.append(sae_dirpath)
+
+    if not matching_sae_dirpaths:
+        raise ValueError(
+            "No matching SAE directory found for params:\n"
+            f"expansion_factor {expansion_factor}\n"
+            f"k {k}\n"
+            f"layer {layer}\n"
+            f"architecture {architecture}"
+        )
+
+    sae_base_path = matching_sae_dirpaths[0]
+
+    if len(matching_sae_dirpaths) > 1:
+        logger.warning(
+            "Multiple matching SAE directories found for params:\n"
+            f"expansion_factor {expansion_factor}\n"
+            f"k {k}\n"
+            f"layer {layer}\n"
+            f"architecture {architecture}\n"
+            f"Using the first matching SAE directory: {sae_base_path}"
+        )
+
+    base_path = sae_base_path / "delphi"
     latents_path = base_path / "latents"
-    explanations_path = base_path / "explanations"
     scores_path = base_path / "scores"
-    neighbours_path = base_path / "neighbours"
     visualize_path = base_path / "visualize"
 
     model = AutoModel.from_pretrained(
@@ -55,7 +120,7 @@ def main(
         token=hf_token,
     )
 
-    raise NotImplementedError("Implement hookpoint_to_sparse_encode and run_cfg")
+    hookpoint_to_sparse_encode = load_hookpoints_and_saes(model, sae_base_path)
 
     run_cfg = RunConfig(
         max_latents=n_latents,
@@ -65,6 +130,8 @@ def main(
             n_tokens=n_tokens,
         ),
     )
+
+    raise NotImplementedError("Implement run_cfg")
 
     nrh = assert_type(
         dict,
@@ -77,4 +144,5 @@ def main(
             nrh,
             latents_path,
             model.tokenizer,
+            transcode=False,
         )
