9d189233ed9d37ef58439c70fd533c8f52bb6883
d0rbu <hacperu2010@gmail.com>
2025-10-03 14:29:47 -0500

    update arguably to support variable length args

--- slurm/kmeans.sh
+++ slurm/kmeans.sh
@@ -1,1 +1,1 @@
-uv run torchrun --nnodes 1 --nproc-per-node 1 exp/kmeans.py main --expansion-factor 16,32,64 --tokens-per-file 100000 --reshuffled-tokens-per-file 1000000 --minibatch-size 100000
+uv run torchrun --nnodes 1 --nproc-per-node 1 exp/kmeans.py main --expansion-factor 16,32,64 --tokens-per-file 2000 --reshuffled-tokens-per-file 10000 --minibatch-size 10000
