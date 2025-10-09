68cc2fa780ae2aa93c667caa3b1e705e4bda4295
Henry Castillo <33704345+d0rbu@users.noreply.github.com>
2025-10-09 10:13:51 -0500

    Increase batch size for SAE evaluation

--- slurm/eval_all_saes.sh
+++ slurm/eval_all_saes.sh
@@ -3,4 +3,4 @@
 # Local script for evaluating all SAEs
 # This runs with minimal resources for testing/development
 
-uv run python -m exp.eval_all_saes --saebench-batchsize 8 --intruder-n-tokens 100000
+uv run python -m exp.eval_all_saes --saebench-batchsize 64 --intruder-n-tokens 100000
