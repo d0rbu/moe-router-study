a009146c609473d76d34085fe683e898a1d4e818
d0rbu <hacperu2010@gmail.com>
2025-10-12 15:36:01 -0500

    update default batchsizes

--- slurm/eval_all_saes.sh
+++ slurm/eval_all_saes.sh
@@ -3,4 +3,4 @@
 # Local script for evaluating all SAEs
 # This runs with minimal resources for testing/development
 
-uv run python -m exp.eval_all_saes --saebench-batchsize 64 --intruder-n-tokens 1000000 --log-level DEBUG
+uv run python -m exp.eval_all_saes --saebench-batchsize 16 --intruder-batchsize 4 --intruder-n-tokens 1000000 --log-level DEBUG
