a88ed3f15b48fd8375d4a1389b8dc034a1b817d5
d0rbu <hacperu2010@gmail.com>
2025-12-12 14:49:01 -0600

    add more o11y for token batch loading

--- exp/eval_intruder.py
+++ exp/eval_intruder.py
@@ -643,7 +643,7 @@ class MultiGPULatentPathsCache(LatentPathsCache):
 
     def run(self, n_tokens: int, tokens: th.Tensor):
         """Run caching using multiple GPUs in parallel."""
-        logger.debug(f"Loading token batches for {n_tokens} tokens")
+        logger.debug(f"Loading token batches from shape {tokens.shape} for {n_tokens} tokens")
 
         token_batches = self.load_token_batches(n_tokens, tokens)
         total_batches = len(token_batches)
