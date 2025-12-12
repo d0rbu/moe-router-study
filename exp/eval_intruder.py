4749444a5d5f8eefddee03161d9cee28815db6c4
d0rbu <hacperu2010@gmail.com>
2025-12-12 16:06:12 -0600

    break down token batching

--- exp/eval_intruder.py
+++ exp/eval_intruder.py
@@ -647,7 +647,16 @@ class MultiGPULatentPathsCache(LatentPathsCache):
             f"Loading token batches from shape {tokens.shape} for {n_tokens} tokens on device {tokens.device} with dtype {tokens.dtype} and batch size {self.batch_size}"
         )
 
-        token_batches = self.load_token_batches(n_tokens, tokens)
+        max_batches = n_tokens // tokens.shape[1]
+        logger.debug(f"Max batches: {max_batches}")
+
+        tokens_to_batch = tokens[:max_batches]
+        logger.debug(f"Tokens to batch: {tokens_to_batch.shape}")
+
+        token_batches = th.split(tokens_to_batch, self.batch_size)
+
+        logger.debug(f"Token batches: {token_batches.shape}")
+
         total_batches = len(token_batches)
 
         # Check if we can resume from existing cache
